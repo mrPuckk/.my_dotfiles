04f84ed7dffb440d5ddc67a9839cac4891e5f970
QuangTuong Phung <quangtuongphung@QuangTuongs-MacBook-Pro.local>
2023-09-13 00:14:05 +0700

    Added MacOS setup for CLI
    Enabling the installation of crucial dependencies via Homebrew, accompanied by the addition of a configuration file for macOS customization.

---
+++ macOS_setup/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Running on OSX"
+
+    echo "Brewing all the things"
+    source install/brew.sh
+
+    echo "Updating OSX settings"
+    source install/osx.sh
+fi
+
+echo "Done."
---
+++ macOS_setup/install/brew.sh
@@ -0,0 +1,60 @@
+# cli tools
+brew install ack #Search tool 
+brew install tree
+brew install wget #File retriever
+brew install starship
+brew install tealdeer #Collaborative cheatsheets for console commands
+brew install wakatime-cli #code stats
+
+# development tools
+## git 
+brew install hub #Add Github support
+brew install gh # Add Github cli
+brew install git
+brew install git-delta #Syntax-highlighting pager for git and diff output
+brew install bat #Clone cat with syntax highlighting
+brew install lazygit
+brew install lazydocker
+
+## 
+brew install fd #find
+brew install fzf #cli fuzzy finder
+brew install lf #terminal file manager
+brew install lsd #clone of ls colorful&icons 
+brew install z #Tracks most-used directories to make cd smarter
+brew install zoxide #navigate your filesystem faster
+brew install exa
+
+brew install reattach-to-user-namespace #reattach process
+brew install tmux
+brew install npm #manage multiple node.js 
+brew install markdown 
+
+# editor
+brew install neofetch
+brew install neovim
+brew install fish 
+
+brew install cmake
+
+# monitor
+brew install bottom #GUI monitor
+
+# espanso 
+brew tap espanso/espanso
+brew install espanso
+
+# window manager 
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
+
+brew tap arl/arl
+brew install gitmux
+
+# cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+#lvim
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+
+#end file
